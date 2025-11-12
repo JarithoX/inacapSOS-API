1b821093a553cc0ba5cf393fa5588d69fa31f538
Jarod Pinto <jarodpintojarol2@gmail.com>
2025-11-11 21:16:53 -0300

    index.js creado

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const cors = require('cors');
+
+const usuariosRoutes = require('./routes/usuario.routes');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+
+app.use(cors({ origin: ['http://localhost:8000'], credentials: true }));
+app.use(express.json());
+
+
+// Rutas de la API
+//app.use('/usuarios', usuariosRoutes);
+
+
+// ping opcional
+app.get('/', (_req, res) => res.status(200).send('API InacapSOS funcionando.'));
+
+// Iniciar el servidor
+app.listen(PORT, () => {
+    console.log(`Server corriendo en http://localhost:${PORT}`);
+    console.log('Rutas disponibles:');
+    console.log(`- http://localhost:${PORT}/usuarios`);
+});
