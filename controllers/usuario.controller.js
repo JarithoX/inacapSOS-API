dd082b60e8bced5780a06a1f21f39d4a15a75828
Jarod Pinto <jarodpintojarol2@gmail.com>
2025-11-11 21:42:47 -0300

    Actualizacion del controlador usuario

--- controllers/usuario.controller.js
+++ controllers/usuario.controller.js
@@ -3,8 +3,9 @@ const { db,admin } = require('../config/firebase');
 // GET /usuarios
 async function getUsuarios(_req, res) {
   try {
-    const snapshot = await db.collection('usuarios').get();
+    const snapshot = await db.collection('usuario').get();
     const usuarios = snapshot.docs.map(doc => ({ id: doc.id, ...doc.data() }));
+    console.log(usuarios);
     return res.status(200).json(usuarios);
   } catch (err) {
     console.error('Error al obtener usuarios:', err);
